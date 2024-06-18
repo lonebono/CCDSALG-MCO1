644baa7df81f807b8b1eb6686a935973a21f6186
rein0214 <jamie_iringan@dlsu.edu.ph>
2024-06-18 15:07:44 +0800

    Add files via upload

--- main.c
+++ main.c
@@ -1,33 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+#include <math.h>
 
 #define SIZE 256
 typedef char string[SIZE]; //just for strings in general
 
-void top(string s, string result) 
+char top (string s)
 {
     int lastSpaceIndex = -1;
     int i;
 
-    if (strlen(s) == 0) 
+    if(strlen(s) == 0)
     {
-        result[0] = '\0';
-    } 
+        return '\0';
+    }
     else
     {
-        for (i = 0; i < strlen(s); i++) 
+        for (i = 0; i < strlen(s); i++)
         {
-            if (s[i] == ' ') 
-            {
-                lastSpaceIndex = i;
-            }
+                if (s[i] == ' ')
+                {
+                    lastSpaceIndex = i;
+                }
         }
-        if (lastSpaceIndex == -1) 
-        {
-            strcpy(result, s);
-        } 
-        else
-        {
-            strcpy(result, &s[lastSpaceIndex + 1]);
-        }
+
+        return s[lastSpaceIndex + 1];
     }
 }
 
@@ -41,121 +40,168 @@ void push(string s, string element)
     {
         if (strlen(s) == 0)
         {
-            strcpy(s, element);
+            s[strlen(s)] = *element;
         }
         else
         {
-            s[strlen(s)] = ' ';
             strcat(s, element);
         }
     }
 }
 
-void pop(string s, string result)
+char pop(string *s)
 {
-    int i;
-    int lastSpaceIndex = -1;
-    string temp;
+    string popped;
+    int i, lastSpaceIndex = -1;
 
-    if (strlen(s) == 0) 
+    if (strlen(*s) == 0)
     {
         printf("\nUnderflow Error!");
-    } 
+        return top(*s);
+    }
     else
     {
-        for (i = 0; i < strlen(s); i++) 
+        for (i = 0; i < strlen(*s); i++)
         {
-            if (s[i] == ' ')
+            if (*s[i] == ' ')
             {
                 lastSpaceIndex = i;
             }
         }
+
         if (lastSpaceIndex != -1)
         {
-            top(s, temp);
-            strcpy(result, temp);
-            s[lastSpaceIndex] = '\0';
-        } 
-        else
-        {
-            top(s, temp);
-            strcpy(result, temp);
-            s[0] = '\0';
-        }
-    }
-}
-
-void queueHead(string s, string result)
-{
-    int firstSpaceIndex = -1;
-    int i;
-
-    if (strlen(s) == 0)
-    {
-        result[0] = '\0';
-    }
-    else
-    {
-        for (i = 0; i < strlen(s); i++)
-        {
-            if (s[i] == ' ')
-            {
-                firstSpaceIndex = i;
-            }
-        }
-        if (firstSpaceIndex == -1)
-        {
-            strcpy(result, s);
+            strcpy(popped, s[lastSpaceIndex + 1]);
+            *s[lastSpaceIndex] = '\0';
         }
         else
         {
-            while(s[i] != ' ')
-            {
-                result[i] = s[i];
-            }
+            strcpy(popped, *s);
+            *s[0] = '\0';
         }
+
+        return *popped;
     }
 }
 
-
-void enqueue(string s, string element)
+void enqueue(string *s, string element)
 {
-    if (strlen(s) == 0)
+
+    if (strlen(*s) == 0)
     {
-        strcpy(s, element);
+        *s[strlen(*s)] = *element;
     }
     else
     {
-        s[strlen(s)] = ' ';
-        strcat(s, element);
+        *s[strlen(*s)] = ' ';
+        strcat(*s, element);
     }
 }
 
-void dequeue(string s, string result)
+char queueHead (string s)
 {
-    string dequeued;
-    int i, ctr = 0;
-    int deleted;
-
-    queueHead(s, dequeued);
     
-    if (dequeued[0] == '\0')
+    if(strlen(s) == 0)
     {
-        printf("\nUnderflow Error!");
+        return '\0';
     }
     else
     {
-        strcpy(result, dequeued);
-        deleted = strlen(result);
-        
-        while (ctr != deleted)
-        {
-            for (i = 0; i < strlen(s); i++)
-            {
-                s[i] = s[i + 1];
-            }
-            ctr++;
-        }
-        s[strlen(s) - 1] = '\0';
+        return s[0];
     }
 }
+
+void charToStr(char c, string *result){
+    *result[0] = c;
+    *result[1] = '\0';
+}
+
+int precedence(char op) {
+    if (op == '^')
+        return 3;
+    else if (op == '/' || op == '*')
+        return 2;
+    else if (op == '+' || op == '-')
+        return 1;
+    else
+        return 0;
+}
+
+void infix_to_postfix(string infix, string *postfix){
+    int i = 0;
+    string temp;
+    string stack = "\0";
+    int token_index;
+    int len = strlen(infix);
+    
+    while (i < len){
+        char c = infix[i];
+        
+        if (isdigit(c)){
+            token_index = 0;
+            while (isdigit(c)){
+                temp[token_index++] = c;
+                i++;
+                c = infix[i];
+            }
+            temp[token_index] = '\0';
+            enqueue(postfix, temp);
+            strcpy(temp, "");
+        }
+        else if (c == '('){
+            charToStr(c, &temp);
+            push(stack, temp);
+            strcpy(temp, "");
+            i++;
+        }
+        else if (c == ')'){
+            while (precedence(queueHead(stack)) > 0){
+                charToStr(pop(&stack), &temp);
+                enqueue(postfix, temp);
+                strcpy(temp, "");
+                i++;
+            }
+            
+            pop(&stack);
+        }
+        else{
+            if (precedence(c) > precedence(top(stack))){
+                charToStr(c, &temp);
+                push(stack, temp);
+                strcpy(temp, "");
+                i++;
+            }
+            else {
+                while (precedence(c) <= precedence(top(stack))){
+                    charToStr(c, &temp);
+                    enqueue(postfix, temp);
+                    strcpy(temp, "");
+                    i++;
+                }
+            }
+                
+        }
+    }
+    while (strlen(stack) != 0) {
+        charToStr(pop(&stack), &temp);
+        enqueue(postfix, temp);
+        strcpy(temp, "");
+    }
+}
+
+
+int main() {
+     string infix;
+     string postfix;
+     
+     printf("Enter an infix expression: ");
+     scanf("%s", infix);
+     
+     infix_to_postfix(infix, &postfix);
+     printf("Postfix expression: %s\n", postfix);
+     
+     
+    
+    return 0;
+}
+
