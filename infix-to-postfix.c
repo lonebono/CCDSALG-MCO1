e63aa864b7a525bda86a03e2fbbd185adec5ba39
rein0214 <jamie_iringan@dlsu.edu.ph>
2024-06-21 04:24:47 +0800

    Update infix-to-postfix.c

--- infix-to-postfix.c
+++ infix-to-postfix.c
@@ -4,62 +4,67 @@
 #include <string.h>
 #include <math.h>
 
-
 #define SIZE 256
-typedef char string[SIZE]; //just for strings in general
+typedef char string[SIZE]; // Defines string type for convenience
+
+// Struct definitions for Stack and Queue
 
 typedef struct {
-    char stackint[SIZE];
-    char top;
+    char stackint[SIZE]; // Stack array for operators
+    char top; // Top index of the stack
 } Stack;
 
 typedef struct {
-    string queue;
-    char front, rear;
+    string queue; // Array to hold postfix expression tokens
+    char front, rear; // Front and rear indices for the queue
 } Queue;
 
-typedef Queue queueArray[SIZE];
+typedef Queue queueArray[SIZE]; // Array of queues to hold tokens of postfix expression
 
 void enqueue(string *s, string element)
 {
-
+    // Function to enqueue (add) an element to the end of a string (used as a queue)
     if (strlen(*s) == 0)
     {
-        strcpy(*s, element);
+        strcpy(*s, element); // If string is empty, copies element directly
     }
     else
     {
-        strcat(*s, " ");
-        strcat(*s, element);
+        strcat(*s, " "); // Otherwise, adds a space separator
+        strcat(*s, element); // Then concatenates the element
     }
 }
 
-char queueHead (string s)
+char queueHead(string s)
 {
-
-    if(strlen(s) == 0)
+    // Function to get the first character of a string (used as a queue)
+    if (strlen(s) == 0)
     {
-        return '\0';
+        return '\0'; // Returns null character if string is empty
     }
     else
     {
-        return s[0];
+        return s[0]; // Otherwise, returns the first character of the string
     }
 }
 
-void charToStr(char c, string *result){
-    strcpy(*result, "");
-    strncat(*result, &c, 1);
+void charToStr(char c, string *result)
+{
+    // Function to convert a character to a string
+    strcpy(*result, ""); // Clears the result string
+    strncat(*result, &c, 1); // Concatenates the character to the result string
 }
 
-int icp(string op) {
+int icp(string op)
+{
+    // Function to return the 'in-coming precedence' of an operator
     if (strcmp(op, "^") == 0)
         return 9;
     else if (strcmp(op, "/") == 0 || strcmp(op, "*") == 0)
         return 8;
     else if (strcmp(op, "+") == 0 || strcmp(op, "-") == 0)
         return 7;
-    else if (strcmp(op, ">") == 0 || strcmp(op, "<") == 0)
+    else if (strcmp(op, ">") == 0 || strcmp(op, "<") == 0 || strcmp(op, ">=") == 0 || strcmp(op, "<=") == 0)
         return 6;
     else if (strcmp(op, "==") == 0 || strcmp(op, "!=") == 0)
         return 5;
@@ -70,10 +75,12 @@ int icp(string op) {
     else if (strcmp(op, "||") == 0)
         return 2;
     else
-        return 0;
+        return 0; // Default precedence for unknown operators
 }
 
-int isp(string op) {
+int isp(string op)
+{
+    // Function to return the 'in-stack precedence' of an operator
     if (strcmp(op, "^") == 0)
         return 9;
     else if (strcmp(op, "/") == 0 || strcmp(op, "*") == 0)
@@ -93,17 +100,18 @@ int isp(string op) {
     else if (strcmp(op, "(") == 0)
         return 1;
     else
-        return 0;
+        return 0; // Default precedence for unknown operators
 }
 
 void top(string s, string result)
 {
+    // Function to get the top element (last added) from a stack (string representation)
     int lastSpaceIndex = -1;
     int i;
 
     if (strlen(s) == 0)
     {
-        result[0] = '\0';
+        result[0] = '\0'; // Returns empty string if stack is empty
     }
     else
     {
@@ -111,49 +119,51 @@ void top(string s, string result)
         {
             if (s[i] == ' ')
             {
-                lastSpaceIndex = i;
+                lastSpaceIndex = i; // Finds the last space in the stack string
             }
         }
         if (lastSpaceIndex == -1)
         {
-            strcpy(result, s);
+            strcpy(result, s); // If no space found, copies entire stack string
         }
         else
         {
-            strcpy(result, &s[lastSpaceIndex + 1]);
+            strcpy(result, &s[lastSpaceIndex + 1]); // Otherwise, copies from after the last space
         }
     }
 }
 
 void push(string s, string element)
 {
-    if (strlen(s) == SIZE -1)
+    // Function to push (add) an element onto a stack (string representation)
+    if (strlen(s) == SIZE - 1)
     {
-        printf("\nOverflow Error!");
+        printf("\nOverflow Error!"); // Checks if stack is full
     }
     else
     {
         if (strlen(s) == 0)
         {
-            strcpy(s, element);
+            strcpy(s, element); // If stack is empty, copies element directly
         }
         else
         {
-            strcat(s, " ");
-            strcat(s, element);
+            strcat(s, " "); // Otherwise, adds a space separator
+            strcat(s, element); // Then concatenates the element
         }
     }
 }
 
 void pop(string s, string result)
 {
+    // Function to pop (remove and return) an element from a stack (string representation)
     int i;
     int lastSpaceIndex = -1;
     string temp;
 
     if (strlen(s) == 0)
     {
-        printf("\nUnderflow Error!");
+        printf("\nUnderflow Error!"); // Checks if stack is empty
     }
     else
     {
@@ -161,91 +171,111 @@ void pop(string s, string result)
         {
             if (s[i] == ' ')
             {
-                lastSpaceIndex = i;
+                lastSpaceIndex = i; // Finsd the last space in the stack string
             }
         }
         if (lastSpaceIndex != -1)
         {
-            top(s, temp);
-            strcpy(result, temp);
-            s[lastSpaceIndex] = '\0';
+            top(s, temp); // Gets the top element
+            strcpy(result, temp); // Copies it to result
+            s[lastSpaceIndex] = '\0'; // Removes the element by replacing space with null terminator
         }
         else
         {
-            top(s, temp);
-            strcpy(result, temp);
-            s[0] = '\0';
+            top(s, temp); // Gets the top element
+            strcpy(result, temp); // Copies it to result
+            s[0] = '\0'; // If no space found, clears the stack by setting first character to null terminator
         }
     }
 }
- 
 
-
-void infix_to_postfix(string infix, string *postfix){
+void infix_to_postfix(string infix, string *postfix)
+{
+    // Function to convert infix expression to postfix expression
     int i;
     string x, temp, topOperator;
-    string stackOperators = "\0";
-    string stackOperands = "\0";
-    string operand = "\0";
+    string stackOperators = "\0"; // Stack to hold operators
+    string stackOperands = "\0"; // Stack to temporarily hold operands
+    string operand = "\0"; // Current operand being formed
     int len = strlen(infix);
 
-    for (i = 0; i < len; i++){
+    for (i = 0; i < len; i++)
+    {
         char c = infix[i];
-        charToStr(c, &temp);
+        charToStr(c, &temp); // Converts character to string
 
-        if (isdigit(c)){
-            strcat(stackOperands, temp);
-            
-            if (!isdigit(infix[i+1])) {
-                enqueue(postfix, stackOperands);
-                strcpy(stackOperands, "");
+        if (isdigit(c))
+        {
+            strcat(stackOperands, temp); // Appends digit to operand stack (for numbers with more than 1 digits e.x 123, 56)
+
+            if (!isdigit(infix[i + 1]))
+            {
+                enqueue(postfix, stackOperands); // Enqueues operand as part of postfix expression
+                strcpy(stackOperands, ""); // Clears operand stack
             }
         }
-        else {
-            if (c == '(') {
-                push(stackOperators, temp);
+        else
+        {
+            if (c == '(')
+            {
+                push(stackOperators, temp); // Pushes '(' onto operator stack
             }
-            else if (c == ')') {
-                top(stackOperators, topOperator);
-                
-                while (icp(topOperator) != 0 && strlen(stackOperators) != 0) {
-                    enqueue(postfix, topOperator);
-                    pop(stackOperators, x);
-                    top(stackOperators, topOperator);
-                }
-                pop(stackOperators, x);
-            }
-            else {
-                strcat(operand, temp);
-                //printf(">%s\n", operand);
+            else if (c == ')')
+            {
+                top(stackOperators, topOperator); // Gets top operator from stack
 
-                if (isdigit(infix[i+1]) || infix[i+1] == '(' || infix[i+1] == ')') {
-                    top(stackOperators, topOperator);
-                    if (icp(operand) > isp(topOperator)) {
-                        push(stackOperators, operand);
+                // Pops operators from stack to postfix until '(' encountered
+                while (icp(topOperator) != 0 && strlen(stackOperators) != 0)
+                {
+                    enqueue(postfix, topOperator); // Enqueues operator to postfix expression
+                    pop(stackOperators, x); // Pops operator from stack
+                    top(stackOperators, topOperator); // Gets new top operator
+                }
+                pop(stackOperators, x); // Pops '(' from stack
+            }
+            else
+            {
+                strcat(operand, temp); // Concatenates characters to form operator (for operators with more than 1 character e.x. !=, &&)
+
+                if (isdigit(infix[i + 1]) || infix[i + 1] == '(' || infix[i + 1] == ')')
+                {
+                    top(stackOperators, topOperator); // Gets top operator from stack
+
+                    // Compares precedence of current operand with top operator in stack
+                    if (icp(operand) > isp(topOperator))
+                    {
+                        push(stackOperators, operand); // Pushes operand onto stack
                     }
-                    else {
+                    else
+                    {
                         top(stackOperators, topOperator);
-                        while ((icp(operand) <= isp(topOperator)) && (strlen(stackOperators) != 0)) {
-                            enqueue(postfix, topOperator);
-                            pop(stackOperators, x);
-                            top(stackOperators, topOperator);
+
+                        // Pops operators to postfix until precedence condition met
+                        while ((icp(operand) <= isp(topOperator)) && (strlen(stackOperators) != 0))
+                        {
+                            enqueue(postfix, topOperator); // Enqueues operator to postfix expression
+                            pop(stackOperators, x); // Pops operator from stack
+                            top(stackOperators, topOperator); // Gets new top operator
                         }
-                        push(stackOperators, operand);
+                        push(stackOperators, operand); // Pushes operand onto stack
                     }
-                    strcpy(operand, "");
+                    strcpy(operand, ""); // Clears operand string for next use
                 }
             }
         }
     }
-    while (strlen(stackOperators) != 0) {
-        pop(stackOperators, x);
-        enqueue(postfix, x);
+
+    // Pops remaining operators in stack to postfix expression
+    while (strlen(stackOperators) != 0)
+    {
+        pop(stackOperators, x); // Pops operator from stack
+        enqueue(postfix, x); // Enqueues operator to postfix expression
     }
 }
 
 void queuedPostfix(queueArray *array, string postfix, int *numElem)
 {
+    // Function to parse postfix expression into an array of queues (for each token)
     int i = 0, j = 0, k = 0;
     while (k < strlen(postfix))
     {
@@ -253,31 +283,32 @@ void queuedPostfix(queueArray *array, string postfix, int *numElem)
         {
             while (postfix[k] != ' ' && postfix[k] != '\0')
             {
-                (*array)[i].queue[j] = postfix[k];
+                (*array)[i].queue[j] = postfix[k]; // Adds character to queue element
                 j++;
                 k++;
             }
-            (*array)[i].queue[j] = '\0'; 
+            (*array)[i].queue[j] = '\0'; // Terminates string in queue element
             i++;
             j = 0;
         }
         else
         {
-            (*array)[i].queue[0] = ' ';
-            (*array)[i].queue[1] = '\0';
+            (*array)[i].queue[0] = ' '; // Handles spaces between tokens
+            (*array)[i].queue[1] = '\0'; // Terminates empty space
             i++;
             k++;
         }
     }
-    *numElem = i; // Update the number of elements
+    *numElem = i; // Updates the number of elements in the array
 }
 
 void printQueued(queueArray *array, int numElem)
 {
+    // Function to print each queue element in the array
     int i;
     for (i = 0; i < numElem; i++)
     {
-        printf("Token in index %d: %s \n", i, (*array)[i].queue);
+        printf("Token in index %d: %s \n", i, (*array)[i].queue); // Prints each token queue
     }
 }
 
@@ -293,38 +324,16 @@ int main(void)
         printf("Enter an infix expression (or QUIT to exit): ");
         scanf("%s", infix);
 
-        if (strcmp(infix, "QUIT") == 0) break;
+        if (strcmp(infix, "QUIT") == 0)
+            break;
 
-        infix_to_postfix(infix, &postfix);
-        printf("Postfix expression: %s\n", postfix);
+        infix_to_postfix(infix, &postfix); // Converts infix to postfix
+        printf("Postfix expression: %s\n", postfix); // Prints postfix expression
 
-        queuedPostfix(&array, postfix, &numElem);
-        printQueued(&array, numElem);
-
-        strcpy(postfix, "");
+        queuedPostfix(&array, postfix, &numElem); // Parses postfix into array of queues
+        printQueued(&array, numElem); // Prints each token queue
     }
 
     return 0;
 }
 
-int main() {
-     
-    string infix;
-    string postfix;
-
-    while (strcmp(infix, "QUIT") != 0) {
-        printf("Enter an infix expression: ");
-
-        scanf("%s", infix);
-        
-        infix_to_postfix(infix, &postfix);
-        
-        printf("Postfix expression: %s\n", postfix);
-        printf("Evaluated Value: \n");
-        printf("\n");
-        
-        strcpy(postfix, "");
-    }
-    
-    return 0;
-}
