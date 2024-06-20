89662e2de03a5e6f3ebec0902b8e41d2754570aa
ZenDLSU <160311924+ZenDLSU@users.noreply.github.com>
2024-06-21 01:04:22 +0800

    Update queue.c

--- queue.c
+++ queue.c
@@ -31,12 +31,14 @@ char queueHead(string s)
 
 // implement the queue code algorithm 
 
-void endsOfQueue(Queue *q) {
+void endsOfQueue(Queue *q) 
+{
     q->front = -1;
     q->rear = 0;
 }
 
-void isQueueEmpty(Queue *q) {
+void isQueueEmpty(Queue *q) 
+{
     if (q->front == -1){
         return 1;
     }
@@ -44,7 +46,8 @@ void isQueueEmpty(Queue *q) {
         return 0;
 }
 
-void isQueueFull(Queue *q) {
+void isQueueFull(Queue *q) 
+{
    if (((q->front == 0 && q->rear == MAX_CHAR - 1) || (q->front == q->rear + 1)))
    {
         return 1;
@@ -53,7 +56,8 @@ void isQueueFull(Queue *q) {
         return 0;
 }
 
-void enqueue(Queue *q, int value) {
+void enqueue(Queue *q, int value) 
+{
     if (isQueueFull == 0) {
         printf("Queue is Full!\n");
     } else {
@@ -64,19 +68,22 @@ void enqueue(Queue *q, int value) {
     }
 }
 
-char dequeue(Queue *q) {
+char dequeue(Queue *q) 
+{
     char value; //returns a char
     if (isQueueEmpty == 0){
         printf("Queue is Empty!\n");
         return -1;
     }
-    else {
+    else 
+    {
         value = q->eval[q->front];
         if(q->front == q->rear - 1) { // reset
             q->front = -1;
             q->rear = 0;
         }
-        else {
+        else 
+        {
             q->front = q->front + 1; // q->front = (q->front + 1) % MAX_CHAR
         }
         return value;
