ac903229d548dfef4f58b90df5128cb6ed253954
ZenDLSU <160311924+ZenDLSU@users.noreply.github.com>
2024-06-20 18:35:29 +0800

    Update general.h

--- general.h
+++ general.h
@@ -1,18 +1,9 @@
 #define SIZE 256
 typedef char string[SIZE]; //just for strings in general
-
-typedef struct {
-    char stackint[SIZE];
-    char top;
-} Stack;
-
-typedef struct {
-    string queue;
-    char front, rear;
-} Queue;
-
 typedef Queue queueArray[SIZE];
 
 void charToStr(char c, string *result)
 int icp(string op)
 int isp(string op)
+void infix_to_postfix(string infix, string *postfix)
+void postfix_to_Queue(queueArray *array, string postfix, int *numElem)
