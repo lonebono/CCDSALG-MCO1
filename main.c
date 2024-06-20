77a949163ba7a6cdc445a4f9c7bfc1c89c479e6b
ZenDLSU <160311924+ZenDLSU@users.noreply.github.com>
2024-06-21 01:03:48 +0800

    Update main.c

--- main.c
+++ main.c
@@ -7,3 +7,28 @@
 #define SIZE 256
 typedef char string[SIZE]; //just for strings in general
 
+int main(void) 
+{
+    string infix;
+    string postfix;
+    queueArray array;
+    int numElem = 0;
+    
+    while (strcmp(infix, "QUIT") != 0) {
+        
+        scanf("%s", infix);
+        
+        infix_to_postfix(infix, &postfix);
+        
+        printf("%s\n", postfix); //infix converted to postfix expression
+        
+        queuedPostfix(&array, postfix, &numElem);
+        
+        printf("Evaluated Value: \n"); //evaluated value
+        printf("\n");
+        
+        strcpy(postfix, "");
+    }
+    
+    return 0;
+}
