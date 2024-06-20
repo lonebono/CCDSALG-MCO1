54120e87abac15a929a28d2e8f078fc6c148f591
ZenDLSU <160311924+ZenDLSU@users.noreply.github.com>
2024-06-21 01:05:53 +0800

    Update general.c

--- general.c
+++ general.c
@@ -3,12 +3,14 @@
 #include <string.h>
 #include general.h
 
-void charToStr(char c, string *result){
+void charToStr(char c, string *result)
+{
     strcpy(*result, "");
     strncat(*result, &c, 1);
 }
 
-int icp(string op) {
+int icp(string op) 
+{
     if (strcmp(op, "^") == 0)
         return 9;
     else if (strcmp(op, "/") == 0 || strcmp(op, "*") == 0)
@@ -29,7 +31,8 @@ int icp(string op) {
         return 0;
 }
 
-int isp(string op) {
+int isp(string op) 
+{
     if (strcmp(op, "^") == 0)
         return 9;
     else if (strcmp(op, "/") == 0 || strcmp(op, "*") == 0)
@@ -52,7 +55,8 @@ int isp(string op) {
         return 0;
 }
 
-void infix_to_postfix(string infix, string *postfix){
+void infix_to_postfix(string infix, string *postfix)
+{
     int i;
     string x, temp, topOperator;
     string stackOperators = "\0";
@@ -60,42 +64,54 @@ void infix_to_postfix(string infix, string *postfix){
     string operand = "\0";
     int len = (int)strlen(infix);
 
-    for (i = 0; i < len; i++){
+    for (i = 0; i < len; i++)
+    {
         char c = infix[i];
         charToStr(c, &temp);
 
-        if (isdigit(c)){
+        if (isdigit(c))
+        {
+
             strcat(stackOperands, temp);
             
-            if (!isdigit(infix[i+1])) {
+            if (!isdigit(infix[i+1])) 
+            {
                 enqueue(postfix, stackOperands);
                 strcpy(stackOperands, "");
             }
         }
-        else {
-            if (c == '(') {
+        else 
+        {
+            if (c == '(') 
+            {
                 push(stackOperators, temp);
             }
-            else if (c == ')') {
+            else if (c == ')') 
+            {
                 top(stackOperators, topOperator);
                 
-                while (icp(topOperator) != 0 && strlen(stackOperators) != 0) {
+                while (icp(topOperator) != 0 && strlen(stackOperators) != 0) 
+                {
                     enqueue(postfix, topOperator);
                     pop(stackOperators, x);
                     top(stackOperators, topOperator);
                 }
                 pop(stackOperators, x);
             }
-            else {
+            else 
+            {
                 strcat(operand, temp);
                 if (isdigit(infix[i+1]) || infix[i+1] == '(' || infix[i+1] == ')') {
                     top(stackOperators, topOperator);
-                    if (icp(operand) > isp(topOperator)) {
+                    if (icp(operand) > isp(topOperator)) 
+                    {
                         push(stackOperators, operand);
                     }
-                    else {
+                    else 
+                    {
                         top(stackOperators, topOperator);
-                        while ((icp(operand) <= isp(topOperator)) && (strlen(stackOperators) != 0)) {
+                        while ((icp(operand) <= isp(topOperator)) && (strlen(stackOperators) != 0)) 
+                        {
                             enqueue(postfix, topOperator);
                             pop(stackOperators, x);
                             top(stackOperators, topOperator);
@@ -107,7 +123,8 @@ void infix_to_postfix(string infix, string *postfix){
             }
         }
     }
-    while (strlen(stackOperators) != 0) {
+    while (strlen(stackOperators) != 0) 
+    {
         pop(stackOperators, x);
         enqueue(postfix, x);
     }
