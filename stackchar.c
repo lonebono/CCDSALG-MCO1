11f9aa79000315ca276764f14c546c762ab67aa2
ZenDLSU <160311924+ZenDLSU@users.noreply.github.com>
2024-06-21 01:05:00 +0800

    Update stackchar.c

--- stackchar.c
+++ stackchar.c
@@ -87,37 +87,44 @@ void pop(string s, string result)
 }
 
 //
-void createStack(Stack *s){
+void createStack(Stack *s)
+{
     s->top = -1;
 }
 
-int isStackFull(Stack *s){
+int isStackFull(Stack *s)
+{
     if (s->top == MAX_CHAR - 1)
         return 1;
     else 
         return 0;
 }
 
-int isStackEmpty(Stack *s){
+int isStackEmpty(Stack *s)
+{
     if (s->top == -1)
         return 1;
     else 
         return 0;
 }
 
-int top(Stack* s) {
-    if (isEmpty(s)) {
+int top(Stack* s) 
+{
+    if (isEmpty(s)) 
+    {
         printf("Stack is empty\n");
         return -1;
     }
     return s->stackchar[s->top];
 }
 
-void push(Stack *s, char newChar){
+void push(Stack *s, char newChar)
+{
     if(isStackFull == 1){
         printf("Stack Overflow!\n");
     }
-    else{
+    else
+    {
         s->stackchar[++(s->top)] = newChar;
     }
 }
@@ -126,7 +133,8 @@ char pop(Stack *s){
     if(isStackEmpty == 1){
         printf("Stack Underflow!\n");
     }
-    else{
+    else
+    {
         return s->stackchar[(s->top)--];
     }
 }
